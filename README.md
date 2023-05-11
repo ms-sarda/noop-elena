cd5f7116c27f9d27eecceb3d65280b09d3e5f14e
sak2002 <sakshamkumar.1997@gmail.com>
2023-05-11 03:52:47 -0400

    dummy server for front end dev

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # noop-elena
 EleNA - Elevation-based navigation system
+
+To run dummy server
+- create a new venv
+- install all python dependencies from requirements.txt
+- run the server with `python src/server.py`
+- use command to fetch: 
+  - ```curl --location --request POST '0.0.0.0:8000/get_directions' --header 'Content-Type: application/json' --data-raw '{"source":"138 Brittany Manor Drive, Amherst, MA", "destination": "Amherst Commons, Amherst, MA", "deviation":"100", "min_max":"min", "vehicle": "bike"}'```
--- src/server.py
+++ src/server.py
@@ -1,2 +1,40 @@
 """ will start the server -> receive client request -> 
 send request details to orchestrator -> get result -> output to UI """
+from osmnx import graph, distance
+import osmnx
+from flask import Flask, request
+app = Flask(__name__)
+
+
+def get_parameters(json):
+    return json["source"], json["destination"], json["min_max"], json["deviation"], json["vehicle"]
+
+
+@app.route("/get_directions", methods=["POST"])
+def get_directions():
+    source, destination, min_max_route, percent_deviation, vehicle = get_parameters(request.json)
+    g = osmnx.graph_from_place("Amherst, MA, USA", vehicle)
+    source_gdf = osmnx.geocoder.geocode(source)
+    source_node = osmnx.distance.nearest_nodes(g, source_gdf[1], source_gdf[0])
+
+    dest_gdf = osmnx.geocoder.geocode(destination)
+    dest_node = osmnx.distance.nearest_nodes(g, dest_gdf[1], dest_gdf[0])
+
+    path = osmnx.shortest_path(g, source_node, dest_node)
+    # fig = osmnx.plot.plot_graph_route(g, path)
+    coords = []
+    for i in path:
+        coords.append((g.nodes[i]['y'], g.nodes[i]['x']))
+
+    res = {
+        "directions": coords,
+        "source": [source_gdf[0], source_gdf[1]],
+        "destination": [dest_gdf[0], dest_gdf[1]],
+        "distance": 99,
+        "elevation": 99
+    }
+
+    return res
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
