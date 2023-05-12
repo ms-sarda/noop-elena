6f5120390ef0305da4a5b308be6e7ba9147b1e73
sak2002 <sakshamkumar.1997@gmail.com>
2023-05-12 02:27:18 -0400

    Added common osmnx utils to osmnx connector

--- src/connectors/osmnx_connector.py
+++ src/connectors/osmnx_connector.py
@@ -1,33 +1,49 @@
 # import networkx as nx
+import os.path
+
 import osmnx as ox
+import logging
+
 
 # %matplotlib inline
 # ox.__version__
 
 # TODO check if needs to be class, depends how handling multithreading
 
-def get_city_map(city, state):
-    # check in cache, generate if not
-    city_graph = get_map_from_cache(city, state)
-    if city_graph is None:
-        city_graph = generate_city_map(city, state)
-        # cache it
-    return city_graph
+class OsmnxConnector:
 
+    def get_graph_nodes(self, graph, lat: float, long: float):
+        node, distance = os.distance.nearest_nodes(graph, long, lat, return_dist=True)
+        return node, distance
 
-def get_map_from_cache(city, state):
-    return None
+    def get_lat_long(self, address: str):
+        if address is None:
+            raise Exception("Address cannot be None")
+        return ox.geocoder.geocode(address)
 
+    def get_city_map(self, city, state, country, vehicle):
+        # check in cache, generate if not
+        city_graph = self.get_map_from_cache(city, state, country, vehicle)
+        if city_graph is None:
+            city_graph = self.generate_city_map(city, state, country, vehicle)
+            # cache it
+        return city_graph
 
-def generate_city_map(city, state):
-    # call osmnx lib and generate graph
-    place = {"city": city, "state": state, "country": "USA"}
-    # TODO change to walk
-    #print("getting graph")
-    G = ox.graph_from_place(place, network_type="bike", truncate_by_edge=True)
-    #fig, ax = ox.plot_graph(G, figsize=(10, 10), node_size=0, edge_color="y", edge_linewidth=0.2)
-    #print("got graph")
-    #print(fig, ax)
+    def get_map_from_cache(self, city, state, country, vehicle):
+        filepath = "./" + city + "_" + state + "_" + country + "_" + vehicle + ".graphml"
+        if not os.path.isfile(filepath):
+            return None
+        g = ox.io.load_graphml(filepath=filepath)
+        return g
 
+    def cache_map(self, G, city, state, country, vehicle):
+        filepath = "./" + city + "_" + state + "_" + country + "_" + vehicle + ".graphml"
+        ox.io.save_graphml(G, filepath=filepath)
 
-generate_city_map("Amherst", "Massachusetts")
+    def generate_city_map(self, city: str, state: str, country: str = "USA", vehicle: str = "walk"):
+        # call osmnx lib and generate graph
+        place = {"city": city, "state": state, "country": country}
+        logging.debug("Generating city map for " + str(place))
+        G = ox.graph_from_place(place, network_type=vehicle, truncate_by_edge=True)
+        self.cache_map(G, city, state, country, vehicle)
+        return G
