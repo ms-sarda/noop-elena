6a60b81f63eeafe399cfeec04930a28bb645b801
sak2002 <sakshamkumar.1997@gmail.com>
2023-05-13 16:22:01 -0400

    added placeholders for graph utils. added a get results to map model to get final results

---
+++ src/connectors/graph_utils.py
@@ -0,0 +1,8 @@
+def get_shortest_path(city_map, source_lat_long, destination_lat_long):
+    # TODO Djikstra's implementation
+    return
+
+
+def get_elevation_path(city_map, source_lat_long, destination_lat_long, min_max, deviation, shortest_path_length):
+    # TODO implementation to get final deliverable path
+    return
--- src/model/map_model.py
+++ src/model/map_model.py
@@ -1,4 +1,5 @@
 import connectors.osmnx_connector as ox_cnc
+import connectors.graph_utils as g_u
 
 
 class MapModel:
@@ -11,11 +12,35 @@ class MapModel:
         self.city_map, in_cache = ox_cnc.get_city_map(city, state, country, vehicle)
         if not in_cache:
             ox_cnc.cache_map(self.city_map, city, state, country, vehicle)
+        self.shortest_path = []
+        self.shortest_path_length = float('inf')
+        self.elevation_path = []
+        self.elevation_path_length = float('inf')
+        self.elevation = 0
+        self.source_lat_long = []
+        self.destination_lat_long = []
 
     def get_shortest_path(self):
         # TODO : implement get shortest path method
-        return
+        self.shortest_path, self.shortest_path_length = g_u.get_shortest_path(self.city_map, self.source_lat_long,
+                                                                              self.destination_lat_long)
 
     def get_path(self, min_max: str = "max", deviation: float = 0.0):
         # TODO : implement get path method
-        return
+        self.elevation_path, self.elevation_path_length = g_u.get_elevation_path(self.city_map, self.source_lat_long,
+                                                                                 self.destination_lat_long, min_max,
+                                                                                 deviation, self.shortest_path_length)
+
+    def get_results(self):
+        res = {
+            "shortest_path_directions": self.shortest_path,
+            "elevation_path_directions": self.elevation_path,
+            "source": [self.source_lat_long[0], self.source_lat_long[1]],
+            "destination": [self.destination_lat_long[0], self.destination_lat_long[1]],
+            "shortest_path_distance": self.shortest_path_length,
+            "elevation_path_distance": self.elevation_path_length,
+            "elevation": self.elevation
+        }
+
+        return res
+
