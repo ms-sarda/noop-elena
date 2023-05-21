c9d25593f92683b539b193ec0a98a4fb05b6f578
Nidhi Chandra <nidhichandra@vl965-172-31-252-181.wireless.umass.edu>
2023-05-21 15:02:38 -0400

    Added test case for get_path method

--- src/test_map_model.py
+++ src/test_map_model.py
@@ -26,6 +26,30 @@ def test_get_shortest_path():
 
 
 def test_get_path():
+    with open("tests_examples/tests.json","r") as f:
+        test_case = json.load(f)["get_path"][0]
+    
+    source_input = test_case["input"]["source"]
+    destination_input = test_case["input"]["destination"]
+
+    parsed_location = misc_utils.parse_location(source_input)
+    city = parsed_location["city"]
+    state = parsed_location["state"]
+    country = parsed_location["country"]
+    vehicle = test_case["input"]["vehicle"]
+
+    min_max = test_case["input"]["min_max"]
+    deviation = test_case["input"]["deviation"]
+
+
+    map_model = MapModel(
+            source_input , destination_input, city, state, country, vehicle
+        )
+    map_model.get_path(min_max, deviation)
+    assert map_model.elevation_path_length == test_case["output"]["elevation_path_length"]
+    assert map_model.elevation_path_elevation == test_case["output"]["elevation_path_elevation"]
+    
+
     
 if __name__=="__main__":
-    test_get_shortest_path()
+    test_get_path()
