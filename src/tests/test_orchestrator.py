e20158d5ec6242b38c6572f15a922f6f08d38838
Nidhi Chandra <nidhichandra@umass.edu>
2023-05-22 17:43:20 -0400

    Test cases for validating input to orchestrator module

--- src/tests/test_orchestrator.py
+++ src/tests/test_orchestrator.py
@@ -9,11 +9,79 @@ def test_orchestrator_validate_same_city():
     destination = "79 Brighton Ave, Allston, MA, USA"
     min_max = "max"
     deviation = 150
-    vehicle = "walk"
+    transportation = "walk"
 
-    src_city = utils.parse_location(source)
-    dest_city = utils.parse_location(destination)
-
-    results = orchestrator.compute_path(source, destination, min_max, vehicle, deviation)
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
     print(results)
     assert results["error"] == "Source and Destination not in the same city. Cannot generate path"
+
+#Test Case 2: When input source is null, the code should throw an error
+def test_orchestrator_validate_source_input():
+    orchestrator = Orchestrator()
+
+    source = None
+    destination = "79 Brighton Ave, Allston, MA, USA"
+    min_max = "max"
+    deviation = 150
+    transportation = "walk"
+
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
+    print(results)
+    assert results["error"] == "Source cannot be Null.Please enter correct value."
+
+#Test Case 3: When input destination is null, the code should throw an error
+def test_orchestrator_validate_destination_input():
+    orchestrator = Orchestrator()
+
+    source = "129 Brittany Manor Drive, Amherst, MA, USA"
+    destination = None
+    min_max = "max"
+    deviation = 150
+    transportation = "walk"
+
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
+    print(results)
+    assert results["error"] == "Destination cannot be Null.Please enter correct value."
+
+#Test Case 4: When min_max input is invalid, the code should throw an error
+def test_orchestrator_validate_minmax_input():
+    orchestrator = Orchestrator()
+
+    source = "129 Brittany Manor Drive, Amherst, MA, USA"
+    destination = "79 Brighton Ave, Allston, MA, USA"
+    min_max = "invalid"
+    deviation = 150
+    transportation = "walk"
+
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
+    print(results)
+    assert results["error"] == "Elevation gain can only be maximized or minimized.Please enter correct value."
+
+
+#Test Case 5: When transportation input is invalid, the code should throw an error
+def test_orchestrator_validate_transportation_input():
+    orchestrator = Orchestrator()
+
+    source = "129 Brittany Manor Drive, Amherst, MA, USA"
+    destination = "79 Brighton Ave, Allston, MA, USA"
+    min_max = "min"
+    deviation = 150
+    transportation = "invalid"
+
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
+    print(results)
+    assert results["error"] == "Transport can be walk, bike or drive.Please enter correct value."
+
+#Test Case 6: When deviation input is invalid, the code should throw an error
+def test_orchestrator_validate_deviation_input():
+    orchestrator = Orchestrator()
+
+    source = "129 Brittany Manor Drive, Amherst, MA, USA"
+    destination = "79 Brighton Ave, Allston, MA, USA"
+    min_max = "min"
+    deviation = 300
+    transportation = "walk"
+
+    results = orchestrator.compute_path(source, destination, min_max, transportation, deviation)
+    print(results)
+    assert results["error"] == "Deviation should lie between 100 and 200.Please enter correct value."
