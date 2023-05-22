d4047a2adbdcaf9b5cbea92ae29f61391258c3da
Nidhi Chandra <nidhichandra@umass.edu>
2023-05-22 19:33:09 -0400

    Added test cases for osmnx connector

---
+++ src/tests/test_osmnx_connector.py
@@ -0,0 +1,38 @@
+import connectors.utils as utils
+import connectors.osmnx_connector as ox_cnc
+
+def test_get_lat_long():
+    address = "129 Brittany Manor Drive, Amherst, MA, USA"
+    latitude, longitude = ox_cnc.get_lat_long(
+            address
+        )
+    
+    assert latitude == 42.349458
+    assert longitude == -72.528342
+
+def test_get_graph_nodes():
+    source = "129 Brittany Manor Drive, Amherst, MA, USA"
+    destination = "667 N Pleasant St, Amherst, MA, USA"
+    transportation = "walk"
+
+    city_details = utils.parse_location(source)
+
+    city_map = ox_cnc.get_city_map(
+            city_details["city"],
+            city_details["state"],
+            city_details["country"],
+            transportation,
+        )
+    
+    source_lat_long = ox_cnc.get_lat_long(source)
+    destination_lat_long = ox_cnc.get_lat_long(destination)
+
+    source_node = ox_cnc.get_graph_nodes(
+            city_map, source_lat_long[0], source_lat_long[1]
+        )
+    destination_node = ox_cnc.get_graph_nodes(
+            city_map, destination_lat_long[0], destination_lat_long[1]
+        )
+    print(source_node, destination_node)
+    assert source_node == (66634686, 16.471150769569732)
+    assert destination_node == (2264382590, 0.7237707786983189)
